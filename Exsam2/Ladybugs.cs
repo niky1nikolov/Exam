8e2e14a3e695c8fc5f30812e59be0848710e7b1a
niky1nikolov <niky1_nikolov@mail.bg>
2018-06-25 08:09:49 +0300

    Exma Dictyonary end regex

---
+++ Exsam2/Ladybugs.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace Ladybugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int siseArray =int.Parse(Console.ReadLine());
+
+            int[] indexArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] ladybugIndex = new int[siseArray];
+
+            foreach (var index in indexArray)
+            {
+                ladybugIndex[index] = 1;
+            }
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split();
+
+                if (commands[0] == "end")
+                {
+                    break;
+                }
+
+                string command = commands[1];
+                int indexNewpositions = int.Parse(commands[2]);
+                int positions = int.Parse(commands[0]);
+
+                ladybugIndex[positions] = 0;
+
+                if (command == "right")
+                {
+                    while (true)
+                    {
+                        int indexNew = positions + indexNewpositions;
+
+                        if (indexNew >= siseArray)
+                        {
+                            break;
+                        }
+
+                        if (ladybugIndex[indexNew] != 1)
+                        {
+                            ladybugIndex[indexNew] = 1;
+                            break;
+                        }
+                        else
+                        {
+                            indexNewpositions += indexNewpositions;
+                        }
+                    }
+                }
+                else if (command == "left")
+                {
+                    while (true)
+                    {
+                        int indexNew = positions - indexNewpositions;
+
+                        if (indexNew < 0)
+                        {
+                            break;
+                        }
+
+                        if (ladybugIndex[indexNew] != 1)
+                        {
+                            ladybugIndex[indexNew] = 1;
+                            break;
+                        }
+                        else
+                        {
+                            indexNewpositions -= indexNewpositions;
+                        }
+                    }
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", ladybugIndex));
+            
+        }
+    }
+}
---
+++ Exsam2/NetherRealms.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] demons = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            SortedDictionary<string, int> demonHelths = new SortedDictionary<string, int>();
+            SortedDictionary<string, double> demonDamages = new SortedDictionary<string, double>();
+            string patter = @"-?\d+\.?\d*";
+
+            Regex regex = new Regex(patter);
+
+            foreach (var demon in demons)
+            {
+                var health = demon.Where(s => !char.IsDigit(s) && s != '+' && s != '-' && s != '*' && s != '/' && s != '.').Sum(s => s);
+
+                demonHelths[demon] = health;
+
+                MatchCollection matches = regex.Matches(demon);
+
+                double damage = 0.0;
+
+                foreach (Match mach in matches)
+                {
+                    var value = mach.Value;
+                    damage += double.Parse(value); 
+                }
+
+                foreach (var symbol in demon)
+                {
+                    if(symbol == '*')
+                    {
+                        damage *= 2;
+                    }
+                    else if(symbol =='/')
+                    {
+                        damage /= 2;
+                    }
+                }
+
+                demonDamages[demon] = damage;
+            }
+
+            foreach (var demon in demonDamages)
+            {
+                var demonName = demon.Key;
+                var demonHealth = demonHelths[demonName];
+                var demonDamage = demon.Value;
+
+                Console.WriteLine($"{demonName} - {demonHealth} health, {demonDamage:F2} damage");
+            }
+        }
+    }
+}
